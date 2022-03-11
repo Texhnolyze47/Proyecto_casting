7e3f066496c2b898b07100077011507f8f933894
Texhnolyze <ichigobankai136@gmail.com>
2022-03-11 09:34:01 -0600

    dividindo en clases

--- src/main/java/com/mx/datos/Conexion.java
+++ src/main/java/com/mx/datos/Conexion.java
@@ -1,5 +1,21 @@
 package com.mx.datos;
 
-public class Conexion {
+import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+import com.mysql.cj.protocol.Resultset;
+
+public class Conexion {
+	private static final String  JDBC_URL =  "jdbc:mysql://localhost:3306/usuarios_casting?useSSL=false&useTimezone=UTC&allowPublicKeyRetrieval=true";
+	private static final String  JDBC_USER =  "root";
+	private static final String  JDBC_PASSWORD =  "admin";
+	
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(JDBC_URL,JDBC_USER,JDBC_PASSWORD);
+	}
+	
+	public static void close(Resultset rs) {
+		
+	}
 }
--- src/main/java/com/mx/test/TestMSql.java
+++ src/main/java/com/mx/test/TestMSql.java
@@ -11,7 +11,8 @@ public class TestMSql {
 
 	public static void main(String[] args) {
 		
-		String url =  "jdbc:mysql://localhost:3306/usuarios_casting?useSSL=false&useTimezone=UTC&allowPublicKeyRetrieval=true";
+		 String  url =  "jdbc:mysql://localhost:3306/usuarios_casting?useSSL=false&useTimezone=UTC&allowPublicKeyRetrieval=true";
+		
 		try {
 			Connection conexion = DriverManager.getConnection(url,"root","admin");
 			java.sql.Statement instruccion = conexion.createStatement(); 
