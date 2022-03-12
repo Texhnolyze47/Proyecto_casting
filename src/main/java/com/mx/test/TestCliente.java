b011b8d5c2efeba0cf5dbad85ab53c085b594211
Texhnolyze <ichigobankai136@gmail.com>
2022-03-11 20:35:10 -0600

    add: metodo de insertar

--- src/main/java/com/mx/test/TestCliente.java
+++ src/main/java/com/mx/test/TestCliente.java
@@ -7,8 +7,14 @@ import com.mx.domain.Cliente;
 
 public class TestCliente {
 	public static void main(String[] args) {
-		ClienteDao clienteDato = new ClienteDao();
-		List<Cliente> clientes = clienteDato.selecccionar();
+		
+		ClienteDao clienteDao = new ClienteDao();
+		List<Cliente> clientes = clienteDao.selecccionar();
+		
+		Cliente clienteNuevo =  new Cliente("Crysta Miche", "1 Heffernan Alley"  , "698-545-0539", "Chev Bartlet", "Analyst Programmer");
+		clienteDao.insertar(clienteNuevo);
+		
+		
 		for(Cliente cliente: clientes) {
 			System.out.println("Persona = " + cliente);
 		}
