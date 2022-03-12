a455d325d1add058c0255936d70b4b31b324b770
Texhnolyze <ichigobankai136@gmail.com>
2022-03-11 19:08:43 -0600

    Se termino la conexion de Cliente dao

--- src/main/java/com/mx/datos/ClienteDao.java
+++ src/main/java/com/mx/datos/ClienteDao.java
@@ -2,14 +2,15 @@ package com.mx.datos;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.mx.domain.*;
+import com.mx.domain.Cliente;
 
 public class ClienteDao {
 	
-	private static final String  SQL_SELECT =  "";
+	private static final String  SQL_SELECT =  "SELECT id, nombre, direccion, telefono, persona-contacto, actividad";
 	
 	public List<Cliente> selecccionar(){
 		Connection conn = null;
@@ -23,17 +24,28 @@ public class ClienteDao {
 			stmt = conn.prepareStatement(SQL_SELECT);
 			rs = stmt.executeQuery();
 			while (rs.next()) {
-				int idPersona = rs.getInt("id_persona");
+				int idPersona = rs.getInt("id");
 				String nombreC = rs.getString("nombre");
 				String direccion = rs.getString("direccion");
-				String telefono = rs.getNString("persona-contacto");
+				String contacto = rs.getNString("persona-contacto");
 				String actividad = rs.getString("actividad");
-				cliente = new Cliente(idPersona, nombreC, direccion, telefono,actividad);
-		
+				cliente = new Cliente(idPersona, nombreC, direccion, contacto,actividad);
+				
+				clientes.add(cliente);
 				
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
+		}
+		finally {
+			try {
+				Conexion.closeResultSet(rs);
+				Conexion.closeStatement(stmt);
+				Conexion.closeConnection(conn);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace(System.out);
+			}
 		}
 		return clientes;
 		
